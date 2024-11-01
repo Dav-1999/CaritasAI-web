36de11cee0084806a0304d42f8b44b5aa00f4314
wzj042 <xqher@foxmail.com>
2024-11-01 13:18:52 +0800

    feat: 修改svg组件拼接方式，添加使用例子

--- src/components/CounterComp.vue
+++ src/components/CounterComp.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <button @click="add"><svg-icon icon-class="sailboat" />increase</button>
+    <svg-icon icon-class="up" color="tomato" />
   </div>
 </template>
 
@@ -17,6 +18,7 @@ button {
   padding: 1rem 2rem;
   border: none;
   border-radius: 2rem;
+  color: teal;
   background-color: azure;
   cursor: pointer;
   opacity: 0.8;
