5f71c43ed226a9a456ac27e99d828d487f2195fc
wzj042 <xqher@foxmail.com>
2024-11-03 12:53:20 +0800

    feat: 测试无限滚动加载列表

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,24 @@
 // 封装网络请求相关
 
+import type { Article } from '@/types/subject'
 import { createFetch } from '@vueuse/core'
 
 export const clientFetch = createFetch({
   baseUrl: import.meta.env.VITE_APP_BASE_URL,
 })
+
+export const getArticle = async (id: number) => {
+  const { data } = await clientFetch<ResponseData<Article>>(`/article/getArticleById?id=${id}`).get().json()
+  return data.value
+}
+
+export const getRelationCount = async () => {
+  const { data } = await clientFetch<ResponseData<number>>('/subjectSubProblemArticle/getSubjectSubProblemArticleListCount').get().json()
+  return data.value.data
+}
+
+export const getRelationList = async (pageNum: number = 0, pageSize: number = 10) => {
+  const { data } = await clientFetch<ResponseData>(`/subjectSubProblemArticle/getSubjectSubProblemArticleList?pageNum=${pageNum}&pageSize=${pageSize}`).get().json()
+  return data.value.data
+}
+
--- src/views/SubjectView.vue
+++ src/views/SubjectView.vue
@@ -6,12 +6,32 @@
         子问题
       </button>
     </header>
+
     <main>
-      <article>内容区域
-        <p v-if="subjectResp.isFetching">loading</p>
-        <pre v-else style="font-size: small;">
-            {{ subjectResp.data }}
-        </pre>
+
+      <div class="status"
+        style="position: absolute; bottom: 0; left: 0; background-color: teal; color: white; width: 20%; height: 40%;">
+        内容总高度<br /> {{ listContainer?.scrollHeight }} <br /><br />
+        当前滚动位置 <br />{{ listContainer?.scrollTop }}<br /><br />
+        可见区域高度<br />{{ listContainer?.clientHeight }}<br /><br />
+      </div>
+      <article ref="listContainer">
+        <div class="article-container">
+          <div class="card" v-for="article in pageData.articleList" :key="article.id">
+
+            <img class="cover" :src="'https://cdn.caritas.pro/image/' + article.id + '.jpg'" alt="cover" />
+            <div class="desp">
+              <p class="title">{{ article.title }}</p>
+              <p class="content">{{ article.content?.slice(0, 140) }}……</p>
+              <div class="meta">
+                <span>{{ article.author }}</span>
+                <span>{{ article.lastUpdate }}</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </article>
       <aside :style="{ display: subproblem_list_collasped ? 'none' : 'flex' }">
         子问题区域
@@ -21,18 +41,72 @@
 </template>
 
 <script setup lang="ts">
+import type { SSPA_Relation, Subject, Article } from '@/types/subject'
+
 const subproblem_list_collasped = ref(true)
+const pageData = ref({
+  relationCount: 0,
+  pageNum: 10,
+  relationList: [] as SSPA_Relation[],
+  subjectListResp: [] as Subject[],
+  articleList: [] as Article[],
+})
+const listContainer = ref<HTMLElement | null>(null)
+useInfiniteScroll(listContainer,
+  async () => {
+
+    console.log(listContainer.value?.scrollTop)
+    pageData.value.pageNum += 1
+    console.log(pageData.value.pageNum);
+    console.log('rel size', pageData.value.relationList.length);
+    console.log('article size', pageData.value.articleList.length);
 
 
-const getSubjectList = () =>{
-  const { isFetching, error, data } = useFetch<ResponseData>('https://httpbin.org/delay/4').get()
-  return ref({ isFetching, error, data })
-}
 
-const subjectResp = getSubjectList()
+    const relationList = await getRelationList(pageData.value.pageNum)
+    pageData.value.relationList = relationList
+    relationList.forEach(async (item: SSPA_Relation) => {
+      pageData.value.articleList.push(await getArticle(item.articleId))
+    })
+    // 打印当前滚动位置
+    console.log(listContainer.value?.scrollTop)
+
+    console.log(pageData.value.pageNum);
+    console.log('before rel size', pageData.value.relationList.length);
+    console.log('article size', pageData.value.articleList.length);
+  },
+  {
+    distance: 600,
+    interval: 2000,
+    direction: 'bottom',
+    canLoadMore: () => true,
+  },
+)
+
+// const onLoadMore = async () => {
+//   pageData.value.pageNum += 1
+//   const relationList = await getRelationList(pageData.value.pageNum)
+//   pageData.value.relationList = relationList
+//   relationList.forEach(async (item: SSPA_Relation) => {
+//     pageData.value.articleList.push(await getArticle(item.articleId))
+//   })
+// }
+
+onMounted(async () => {
+
+  pageData.value.relationCount = await getRelationCount();
+  // const relationList = await getRelationList(pageData.value.pageNum)
+  // pageData.value.relationList = relationList
+  // relationList.forEach(async (item: SSPA_Relation) => {
+  //   pageData.value.articleList.push(await getArticle(item.articleId))
+  // })
+})
+
 </script>
 
 <style scoped>
+@import url('../styles/subject.css');
+
 .subject-container {
   display: flex;
   flex-direction: column;
@@ -42,7 +116,6 @@ const subjectResp = getSubjectList()
 header {
   display: flex;
   justify-content: space-between;
-  align-items: center;
   padding: 0 20px;
   height: 50px;
   background-color: tomato;
@@ -55,7 +128,9 @@ main {
 
 article {
   flex: 4;
-  background-color: cadetblue;
+  max-height: calc(100vh - 50px);
+  overflow-y: auto;
+  background-color: azure;
 }
 
 aside {
